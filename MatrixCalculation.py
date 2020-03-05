287476ddb3b92825b1ed1db3fb7b9cd18359c912
Aida van de Wetering <aida@osphire.com>
2020-03-05 17:07:50 +0100

    Fixed C1 matrix

--- MatrixCalculation.py
+++ MatrixCalculation.py
@@ -1,6 +1,6 @@
 import control as ctrl
+from Cit_par import *
 import numpy as np
-from Cit_par.py import *
 
 ## Composite Matrices from appendix D
 # These have been rewritten in terms of State variable u,a,theta,q.
@@ -9,19 +9,19 @@ from Cit_par.py import *
 cbar = c
 V = V0
 
-C1 = np.array([ [(-2*muc*cbar/V**2), 0, 0,0],
-                [0, (CZadot -2*muc), 0, 0],
-                [0, 0, (-cbar/V), 0],
-                [0,(cbar/V*Cmadot)]])
+C1 = np.array([ [(-2*muc*cbar/V**2), 0., 0., 0.],
+                [0., (CZadot -2*muc), 0., 0.],
+                [0., 0., (-cbar/V), 0.],
+                [0., (cbar/V*Cmadot), 0., (-2*muc*KY2*(cbar/V)**2)]])
 
 C2 = np.array([ [1/V*CXa, CXa, CZ0, cbar/V*CXa],
-                [1/V*CZa, Cza, -CX0, -cbar/V*(CZa + 2*muc)],
-                [0, 0, 0, cbar/V],
-                [1/V*Cma, Cma, 0,cbar/V*Cmq]])
+                [1/V*CZa, CZa, -CX0, -cbar/V*(CZa + 2*muc)],
+                [0., 0., 0., cbar/V],
+                [1/V*Cmu, Cma, 0., cbar/V*Cmq]])
 
 C3 = np.array([ [CXde],
                 [CZde],
-                [0],
+                [0.],
                 [Cmde]])
 
 ## Define matrices A,B,C,D ##
@@ -34,7 +34,7 @@ C = np.array([  [1/V, 0, 0, 0],
                 [0, 1, 0, 0],
                 [0, 0, 1, 0],
                 [0, 0, 0, cbar/V]])
-D = np.zeros((4,4))
+D = np.zeros((4,1))
 
 
 ## Create state space system ##
