84fde6497118d327d86f88c4a214cdb11df63909
Manouk <manouk.zwan@gmail.com>
2020-03-05 17:04:21 +0100

    Cl graphs

--- CLCDcurve.py
+++ CLCDcurve.py
@@ -1,6 +1,16 @@
 from Cit_par import *
 import numpy as np
+import matplotlib.pyplot as plt
+
+alpha =np.array([1.7, 2.4, 3.6, 8.7, 10.6])
+V = np.array([128.0967, 113.6922, 98.7733, 66.8777, 60.70444])
+CLgraph = W /(0.5 * V**2 * rho * S)
+
+xt = np.polyfit(alpha, CLgraph,1)
+yt = np.poly1d(xt)
+plt.grid()
+fig = plt.plot(xt,yt(xt))
+plt.show()
 
 
 
-alpha =[1.7, 2.4, 3.6, 5.4,8.7,10.6]
