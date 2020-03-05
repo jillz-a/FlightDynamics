c019a9faba9487907254e319d054af40a4b1a4a9
jillz-a <jillz-a@users.noreply.github.com>
2020-03-05 17:19:56 +0100

    Verification.py now works

--- Verification.py
+++ Verification.py
@@ -1,14 +1,13 @@
 import unittest
-
-from MatrixCalculation import *
+import numpy as np
 
 class Test(unittest.TestCase):
 
     def test_eig(self):
-        A = np.array([[1,0,0,0],[1,1,1,1],[1,0,1,1],[1,0,1,1]])
-        eig = np.linalg.eig(A)
-        expected =
-        self.assertTrue(abs(eig - expected)/expected < 0.03)
+        A = np.array([[1,0,0,0],[1,1,0,0],[1,1,1,1],[1,1,1,1]])
+        eig = np.linalg.eigvals(A)
+        expected = [2.,0.,1.,1.]
+        self.assertEqual(list(eig), expected)
 
 if __name__ == '__main__':
     unittest.main()
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 #test
-
+import numpy as np
 #vo
 
 #lol
 
 #ola
+
+A = np.array([[1,0,0,0],[1,1,0,0],[1,1,1,1],[1,1,1,1]])
+eig = np.linalg.eigvals(A)
+print(eig)
