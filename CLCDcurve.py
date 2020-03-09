302ff716b8fd50ecec72ee903034af698c703ea7
Manouk <manouk.zwan@gmail.com>
2020-03-09 14:26:11 +0100

    CD calculation

--- CLCDcurve.py
+++ CLCDcurve.py
@@ -6,12 +6,13 @@ alpha =np.array([1.7, 2.4, 3.6, 8.7, 10.6])
 V = np.array([128.0967, 113.6922, 98.7733, 66.8777, 60.70444])
 CLgraph = W /(0.5 * V**2 * rho * S)
 
-CDgraph = CD0 + (CLgraph * alpha) ** 2 / (pi * A * e)
-print(CDgraph)
+CDgraph = CD0 + (CLgraph * alpha*(pi/180)) ** 2 / (pi * A * e)
 
 
 # xt = np.polyfit(alpha, CLgraph,1)
 # yt = np.poly1d(xt)
-# plt.grid()
+plt.grid()
 # fig = plt.plot(xt,yt(xt))
-# plt.show()
+# plt.plot(alpha,CDgraph)
+plt.plot(CDgraph,CLgraph)
+plt.show()
