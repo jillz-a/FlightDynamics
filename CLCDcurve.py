cfdaa4251d2a212a0c3253705e1ed2f4b3ba6fae
Manouk <manouk.zwan@gmail.com>
2020-03-09 16:19:16 +0100

    CD calculation

--- CLCDcurve.py
+++ CLCDcurve.py
@@ -4,28 +4,30 @@ import matplotlib.pyplot as plt
 from scipy import optimize
 import csv
 
-with open('AOA_VTAS.csv') as csvfile:
+with open('AOA_VTAS.csv', encoding='UTF-8') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     alpha = []
     V = []
     for row in readCSV:
-        alpha = row[0]
-        V = row[1]
-
+        alpha.append(float(row[0]))
+        V.append(float(row[1])*0.51444444444444) #convert knots to m/s
 AOA = np.array(alpha)
 Vtas = np.array(V)
-print(AOA,Vtas)
 
 # alpha =np.array([1.7, 2.4, 3.6, 8.7, 10.6])
 # V = np.array([128.0967, 113.6922, 98.7733, 66.8777, 60.70444])
-# CLgraph = W /(0.5 * V**2 * rho * S)
-# CDgraph = CD0 + (CLgraph * alpha*(pi/180)) ** 2 / (pi * A * e)
+CLgraph = W /(0.5 * Vtas**2 * rho * S)
+CDgraph = CD0 + (CLgraph * AOA * (pi/180)) ** 2 / (pi * A * e)
+print(np.argmax(CLgraph),max(CLgraph))
+print(np.argmin(Vtas),min(Vtas))
 
 # def test_func(x,a,b):
 #     return a*x**4 + b
 # params, params_covariance = optimize.curve_fit(test_func, alpha, CDgraph)
 
-# plt.grid()
+plt.grid()
+plt.scatter(AOA,CLgraph)
+plt.show()
 # # xt = np.polyfit(alpha, CLgraph,1)
 # # yt = np.poly1d(xt)
 # # fig = plt.plot(xt,yt(xt))
