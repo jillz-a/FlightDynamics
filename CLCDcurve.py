dffebc9828475b43df6629fbc74250e56e300fdc
Manouk <manouk.zwan@gmail.com>
2020-03-09 17:31:41 +0100

    Zeer aan het aankloten met random data

--- CLCDcurve.py
+++ CLCDcurve.py
@@ -14,24 +14,19 @@ with open('AOA_VTAS.csv', encoding='UTF-8') as csvfile:
             V.append(float(row[0])*0.51444444444444) #convert knots to m/s
 
 data = np.array([alpha,V])
-data2 = np.argsort(data,axis=0)
-print(data)
+data2 = data[np.argsort(data[:, 0])]
 print(data2)
+# data22 = np.delete(data2,[0:5653], 0)
+# print(len(data22))
 
-CLgraph = W/(0.5 * data[1]**2 * rho * S)
-AOA = data[0]
+CLgraph = W/(0.5 * data2[1]**2 * rho * S)
+AOA = data2[0]
 
 plt.grid()
 plt.scatter(AOA,CLgraph)
 plt.show()
-# # xt = np.polyfit(alpha, CLgraph,1)
-# # yt = np.poly1d(xt)
-# # fig = plt.plot(xt,yt(xt))
-# # plt.plot(alpha,CDgraph)
-# plt.scatter(alpha,CDgraph)
-# plt.plot(alpha,test_func(alpha,params[0],params[1]))
-# plt.ylim(0.0395,0.0418)
-# plt.show()
+
+
 
 ## Comments ##
 # CDgraph = CD0 + (CLgraph * AOA * (pi/180)) ** 2 / (pi * A * e)
@@ -40,3 +35,11 @@ plt.show()
 # params, params_covariance = optimize.curve_fit(test_func, alpha, CDgraph)
 # alpha =np.array([1.7, 2.4, 3.6, 8.7, 10.6])
 # V = np.array([128.0967, 113.6922, 98.7733, 66.8777, 60.70444])
+# # xt = np.polyfit(alpha, CLgraph,1)
+# # yt = np.poly1d(xt)
+# # fig = plt.plot(xt,yt(xt))
+# # plt.plot(alpha,CDgraph)
+# plt.scatter(alpha,CDgraph)
+# plt.plot(alpha,test_func(alpha,params[0],params[1]))
+# plt.ylim(0.0395,0.0418)
+# plt.show()
