d657a4d50e283cbdc2ce7ec101bae0b62c43de11
Manouk <manouk.zwan@gmail.com>
2020-03-11 12:03:54 +0100

    Scatter plot met polyfit om Cl alpha te bepalen

--- CLCDcurve.py
+++ CLCDcurve.py
@@ -9,30 +9,43 @@ with open('AOA_VTAS.csv', encoding='UTF-8') as csvfile:
     alpha = []
     V = []
     for row in readCSV:
-        if float(row[0]) > 100:
+        if float(row[0])>75:
             alpha.append(float(row[1]))
             V.append(float(row[0])*0.51444444444444) #convert knots to m/s
 
 data = np.array([alpha,V])
 data2 = data[np.argsort(data[:, 0])]
-print(data2)
 
+##Calculate CLalpha##
 CLgraph = W/(0.5 * data2[1]**2 * rho * S)
 AOA = data2[0]
+t, m = np.polyfit(AOA,CLgraph,1)
+print('Cl_alpha =', t*(180/pi))
 
+##Calculate CD##
+CDgraph = CD0 + (CLgraph) ** 2 / (pi * A * e)
+
+#Subplots##
+
+
+plt.subplot(131)
 plt.grid()
 plt.scatter(AOA,CLgraph)
-t, m = np.polyfit(AOA,CLgraph,1)
 plt.plot(AOA,t*AOA+m,c='red')
+plt.title('CL-alpha curve')
+
+plt.subplot(132)
+plt.grid()
+plt.scatter(AOA,CDgraph)
+plt.title('CD-alpha curve')
+
+plt.subplot(133)
+plt.grid()
+plt.scatter(CDgraph,CLgraph)
+plt.title('CD-CL curve')
+
 plt.show()
-print(t*(180/pi))
-
-
 ## Comments ##
-# CDgraph = CD0 + (CLgraph * AOA * (pi/180)) ** 2 / (pi * A * e)
-# def test_func(x,a,b):
-#     return a*x**4 + b
-# params, params_covariance = optimize.curve_fit(test_func, alpha, CDgraph)
 # alpha =np.array([1.7, 2.4, 3.6, 8.7, 10.6])
 # V = np.array([128.0967, 113.6922, 98.7733, 66.8777, 60.70444])
 # # xt = np.polyfit(alpha, CLgraph,1)
