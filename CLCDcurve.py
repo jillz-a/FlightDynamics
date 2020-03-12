c001db4647b04bc4bf94dde0f8a9f2f7549678d6
Manouk <manouk.zwan@gmail.com>
2020-03-12 21:55:39 +0100

    Aankloten met pandas

--- CLCDcurve.py
+++ CLCDcurve.py
@@ -1,60 +1,51 @@
 from Cit_par import *
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import optimize
-import csv
+import pandas as pd
+
 ##READ DATA AND CREATE ARRAY##
-variables = ['flight_data/AOA','flight_data/TAS','flight_data/Mach','flight_data/alt','flight_data/bcAlt','flight_data/TAT']
-AOA = []
-TAS = np.empty([48321,1])
-Mach = np.empty([48321,1])
-alt = np.empty([48321,1])
-bcAlt = np.empty([48321,1])
-TAT = np.empty([48321,1])
-names = [AOA,TAS,Mach,alt,bcAlt,TAT]
-for i in variables:
-    f = open(i,'r')
-    q = []
-    for line in f.readlines():
-        q.append(float(line))
-        for j in range(len(names)):
-            names[j] = np.array(q)
-print(names[2])
+AOA = np.array(pd.read_csv('flight_data/AOA.csv', delimiter=' ', header=None))
+TAS = np.array(pd.read_csv('flight_data/TAS.csv', delimiter=' ', header=None))      #TAS in knots
+TAS = TAS * 0.51444444444444
+TAT = np.array(pd.read_csv('flight_data/TAT.csv', delimiter=' ', header=None))
+Mach = np.array(pd.read_csv('flight_data/Mach.csv', delimiter=' ', header=None))
+alt = np.array(pd.read_csv('flight_data/alt.csv', delimiter=' ', header=None))
+bcAlt = np.array(pd.read_csv('flight_data/bcAlt.csv', delimiter=' ', header=None))
+
+AT = np.column_stack([AOA,TAS])
+# AT = [[[AOA[x],TAS[x]] for x in AOA if TAS[x] > 39] for x in TAS]     #filter the outliers for the plots
+AT = [AT[i,:] for i in AT if AT[i,1] > 39.00]
+print(AT)
+
 
 ##Calculate CLalpha##
-# CLgraph = W/(0.5 * data2[1]**2 * rho * S)
-# t, m = np.polyfit(AOA,CLgraph,1)
-# print('Cl_alpha =', t*(180/pi))
-#
-# ##Calculate CD##
-# CDgraph = CD0 + (CLgraph) ** 2 / (pi * A * e)
-#
-# #Subplots##
-# plt.subplot(121)
-# plt.grid()
-# plt.scatter(AOA,CLgraph)
-# plt.plot(AOA,t*AOA+m,c='red')
-# plt.title('CL-alpha curve')
-#
-# plt.subplot(122)
-# plt.grid()
-# plt.scatter(CDgraph,CLgraph)
-# plt.title('CD-CL curve')
-#
-# plt.show()
+CLgraph = W/(0.5 * AT[1]**2 * rho * S)
+t, m = np.polyfit(AT[0],CLgraph,1)
+print('Cl_alpha =', t*(180/pi))
+
+##Calculate CD##
+CDgraph = CD0 + (CLgraph) ** 2 / (pi * A * e)
+
+#Subplots##
+plt.subplot(121)
+plt.grid()
+plt.scatter(AT[0],CLgraph)
+plt.plot(AT[0],t*AT[0]+m,c='red')
+plt.title('CL-alpha curve')
+
+plt.subplot(122)
+plt.grid()
+plt.scatter(CDgraph,CLgraph)
+plt.title('CD-CL curve')
+
+plt.show()
 
 ##Calculate Reynolds##
 
-
 ##COMMENTS##
-# with open('AOA_VTAS.csv', encoding='UTF-8') as csvfile:
-#     readCSV = csv.reader(csvfile, delimiter=',')
-#     alpha = []
-#     V = []
-#     for row in readCSV:
-#         if float(row[0])>75:
-#             alpha.append(float(row[1]))
-#             V.append(float(row[0])*0.51444444444444) #convert knots to m/s
-#
-# data = np.array([alpha,V])
-# data2 = data[np.argsort(data[:, 0])]
+    # alpha = []
+    # V = []
+    # for row in readCSV:
+    #     if float(row[0])>75:
+    #         alpha.append(float(row[1]))
+    #         V.append(float(row[0])*0.51444444444444) #convert knots to m/s
