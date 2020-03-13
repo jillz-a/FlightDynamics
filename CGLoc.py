2461434234c91caa264eaa9b4ab7ed56835ae642
jillz-a <jillz-a@users.noreply.github.com>
2020-03-13 12:59:41 +0100

    op hoop van zegen dat ie mn CG nu niet verwijdert

--- CGLoc.py
+++ CGLoc.py
@@ -1,8 +1,10 @@
 import numpy as np
-
+import pandas as pd
 #======================================initial values (non metric)======================================================
 Ramp_mass = 0 #pounds
 x_cg_ramp = 0 #inches
+
+Fuel_block = 4050 #pounds
 
 x_s1 = 131
 m_s1 = 0
@@ -34,6 +36,44 @@ m_s10 = 0
 #initial values (metric)
 Ramp_mass = Ramp_mass*0.453592 #kg
 x_cg_ramp = x_cg_ramp*0.0254 #m
+
+x_s1 = x_s1 * 0.453592 #m
+m_s1 = m_s1 * 0.0254 #kg
+
+x_s2 = x_s2 * 0.453592 #m
+m_s2 = m_s2 * 0.0254 #kg
+
+x_s3 = x_s3 * 0.453592 #m
+m_s3 = m_s3 * 0.0254 #kg
+
+x_s4 = x_s4 * 0.453592 #m
+m_s4 = m_s4 * 0.0254 #kg
+
+x_s5 = x_s5 * 0.453592 #m
+m_s5 = m_s5 * 0.0254 #kg
+
+x_s6 = x_s6 * 0.453592 #m
+m_s6 = m_s6 * 0.0254 #kg
+
+x_s7 = x_s7 * 0.453592 #m
+m_s7 = m_s7 * 0.0254 #kg
+
+x_s8 = x_s8 * 0.453592 #m
+m_s8 = m_s8 * 0.0254 #kg
+
+x_s10 = x_s10 * 0.453592 #m
+m_s10 = m_s10 * 0.0254 #kg
+
+#================================================Moment contributions==============================================================
+#Empty mass contributions
+M_empty = 2672953.5 * 0.453592 * 0.0254 #Nm
+
+#Payload contribution
+M_pay = x_s1 * m_s1 + x_s2 * m_s2 + x_s3 * m_s3 + x_s4 * m_s4 + x_s5 * m_s5 + x_s6 * m_s6 + x_s7 * m_s7 + x_s8 * m_s8 + x_s10 * m_s10
+
+#Fuel contribution
+#Data from weighing form
+fuel_data = np.array(pd.read_csv('fuel_variation.csv', delimiter=',', header=None))
 
 
 #Steps to calculate ramp mass and x_cg_datum:
